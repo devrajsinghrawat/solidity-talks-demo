9ac4bab5a7123f2cb593e8d03e375de3d70a3d82
Devraj Singh <devraj.singh.rawat83@gmail.com>
2022-09-12 17:57:51 +0800

    basic examples

---
+++ Basics/ReceiveEth.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+// Send ETH
+/**
+* calldata (msg.data) is empty --> receive exist --> call recieve()
+* calldata (msg.data) is not empty --> call fallback
+*
+*
+*/
+contract ReceiveEth {
+    event Log(uint gas);
+    event ReceiveLogged(uint value, uint gasLeft);
+    event FallbackLogged(bytes msgData, uint value, uint gasLeft);
+
+    receive() external payable {
+        // bytes memory data = msg.data; // msg.data does not exist inside recieve funcition
+        emit  ReceiveLogged(msg.value, gasleft());             
+    }
+
+    fallback() external payable {
+        emit FallbackLogged(msg.data, msg.value, gasleft());
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+}
