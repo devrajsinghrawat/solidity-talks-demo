82e64779148b205c46307d8151d595fa15832252
Devraj Singh <devraj.singh.rawat83@gmail.com>
2022-10-26 18:00:47 +0800

    hardhat and project config

---
+++ WIP/DWDAO/hardhat-helper-config.ts
@@ -0,0 +1,3 @@
+export const VOTING_DELAY = 10;    /* 10 block */
+export const VOTING_PERIOD = 14400;   /* 2 day */
+export const QUORUM_PERCENT = 4;      // 4%
--- WIP/DWDAO/hardhat.config.ts
+++ WIP/DWDAO/hardhat.config.ts
@@ -1,11 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-
-const API_KEY_URL = process.env.API_KEY_URL;
-const API_KEY_URL_MUMBAI = process.env.API_KEY_URL_MUMBAI;
-const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -15,21 +10,6 @@ const config: HardhatUserConfig = {
         runs: 200
       }
     }
-  },
-
-  networks: {
-    mumbai: {
-      url: API_KEY_URL_MUMBAI,
-      accounts: [PRIVATE_KEY],
-    },    
-    rinkeby: {
-      url: API_KEY_URL,
-      accounts: [PRIVATE_KEY],
-    },
-    goerli: {
-      url: API_KEY_URL,
-      accounts: [PRIVATE_KEY],
-    },
   },
   
   etherscan: {
