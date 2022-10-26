12147ef3295c7f881e0043957441be9112735051
Devraj Singh <devraj.singh.rawat83@gmail.com>
2022-10-26 18:01:52 +0800

    timelock contract

---
+++ WIP/DWDAO/contracts/Governance/TimeLock.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract Timelock is TimelockController {
+
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors)
+    {
+
+    }
+
+
+
+}
