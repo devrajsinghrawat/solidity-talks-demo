f113298dcf9872072f69bb35d20c4beb5ac1e22e
Devraj Singh <devraj.singh.rawat83@gmail.com>
2022-12-24 22:13:03 +0800

    working test cases for Call

--- WIP/proxySerise/contracts/Callee.sol
+++ WIP/proxySerise/contracts/Callee.sol
@@ -4,15 +4,16 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 import "./ToBeCalled.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract Callee {
+contract Callee is Ownable{
 
   function callAndUpdateContractState(address _contractToBeCalledRef) external {
       console.log("inside call contract", msg.sender);
       (bool success, ) = _contractToBeCalledRef.call(
-          abi.encodeWithSignature("updateState(string,uint256)","Hello", 10));
-       console.log(success);   
+          abi.encodeWithSignature("updateState(string,uint256)","Hello World", 10));
+      console.log(success);  
+      require(success, "function call failed");
   }
 
 }   
